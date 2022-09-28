b53a70ebe565d01b301535ab0d30ba5275cc9a1f
Daniel Herrera <dan_michigan2012@hotmail.com>
2022-09-27 20:39:41 -0600

    basic styling in place

--- index.js
+++ index.js
@@ -1,6 +1,24 @@
-document.getElementById("hello").replaceWith("whats up");
-
 fetch('https://api.github.com/users/herrera-daniel/repos')
-    .then(res => res.json().then(j => console.log(j)));
+    .then(res => res.json().then(j => {
+        console.log(j);
+        j.forEach(repo => {
+            const content = `
+                <div class='repo'>
+                    <a href='${repo.html_url}'>
+                        <div class='repoName'>
+                            ${repo.name}
+                        </div>
+                    </a>
+                    <div class='repoDesc'>
+                        ${repo.description}
+                    </div>
+                    <div class='repoDetails'>
+                        ${repo.forks_count}
+                    </div>
+                </div>
+            `
+            document.getElementById("repos").innerHTML += content;
+        });
+    }));
 
 
